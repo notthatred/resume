007e9eaf01c774df295ce4288cd8a530524531bc
Riccardo Salucco <riccardo@notthatred.com>
2015-07-09 08:21:27 +0200

    Add var bio to my resume

---
+++ js/resumeBuilder.js
@@ -0,0 +1,15 @@
+var bio = {
+	"name": "Riccardo Salucco",
+	"role": "Front-End Web Developer",
+	"contacts": {
+		"mobile": "<a href='tel:333 3333333'>333 3333333</a>",
+		"email": "<a href='mailto:riccardo.salucco@gmail.com'>riccardo.salucco@gmail.com</a>",
+		"github": "<a href='https://github.com/notthatred'>notthatred</a>",
+		"twitter": "<a href='https://twitter.com/RSalucco'>twitter</a>",
+		"linkedIn": "<a href='https://it.linkedin.com/in/riccardosalucco'>View Profile</a>",
+		"location": "<a href='#'>Firenze, Italia</a>"
+	},
+	"welcomeMessage": "",
+	"skills": ["HTML5", "CSS", "Bootstrap", "Photoshop", "illustrator", "Javascript", "Final Cut"],
+	"bioPic": "img/logo.png"
+};
